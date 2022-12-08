c2349efc843caafb5b0ff803668a1923edc8ef10
Juan Pais <jjosepais@gmail.com>
2022-12-08 14:45:05 -0300

    agrego ícono de favorito

---
+++ src/components/Fav/Fav.js
@@ -0,0 +1,27 @@
+import("./Fav.css");
+
+const Fav = () => {
+	const seleccionado = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+
+		if (e.target.style.backgroundColor === "") {
+			e.target.style.backgroundColor = "yellow";
+		} else {
+			e.target.style.backgroundColor = "";
+		}
+	};
+	return (
+		<img
+			onClick={(e) => {
+				seleccionado(e);
+			}}
+			style={{backgroundColor: ""}}
+			className={"fav"}
+			src=".\images\beerFav .png"
+			alt="favorito"
+		/>
+	);
+};
+
+export default Fav;
--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,16 +1,23 @@
 import {Link} from "react-router-dom";
 import "./item.css";
+import Fav from "../Fav/Fav";
 
 const Item = ({item}) => {
 	return (
-		<div className="item">
-			<Link to={`/item/${item.id}`}>
-				<img className="fotoItem" src={item.url} alt="foto de birra"></img>
-			</Link>
-			<h2>{item.nombre}</h2>
-			<p>{item.categoria}</p>
-			<p>{item.descripcion}</p>
-			<Link to={`/item/${item.id}`}>Ver detalle</Link>
+		<div>
+			<Fav />
+
+			<div className="item">
+				<Link to={`/item/${item.id}`}>
+					<img className="fotoItem" src={item.url} alt="foto de birra"></img>
+				</Link>
+				<h2>{item.nombre}</h2>
+				<p>{item.categoria}</p>
+				<p>{item.descripcion}</p>
+			</div>
+			<div>
+				<Link to={`/item/${item.id}`}>Ver detalle</Link>
+			</div>
 		</div>
 	);
 };
