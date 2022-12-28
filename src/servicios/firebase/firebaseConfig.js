587b0a654b58eced10047e7242c8299890f5b4ea
Juan Pais <jjosepais@gmail.com>
2022-12-28 15:32:05 -0300

    creo variables de entorno

--- src/servicios/firebase/firebaseConfig.js
+++ src/servicios/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyC6-BHTNr7jZDK0hNier-umoK1qzcy_RGE",
-	authDomain: "fervere-e-commerce.firebaseapp.com",
-	projectId: "fervere-e-commerce",
-	storageBucket: "fervere-e-commerce.appspot.com",
-	messagingSenderId: "101553792770",
-	appId: "1:101553792770:web:1649d6eeb96b264a27041b",
+	apiKey: process.env.REACT_APP_apiKey,
+	authDomain: process.env.REACT_APP_authDomain,
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
 };
 
 const app = initializeApp(firebaseConfig);
